c58658b3383fcd6e330735ece8f5f55f9dc87d88
unknown <Ignacio_rato@hotmail.com>
2022-08-13 03:02:13 -0300

    tiendaIgnacio

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import "./components/Navbar/alien.css"
+import  NavBar  from './components/Navbar/Navbar';
+
+function App() {
+  return(
+    <NavBar/>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light color" >
+            <a className="fuente navbar-brand" id="fuente">ALIEN SHOP'S</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li>
+                        <a className="nav-link" href="#">Comidas</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Bebidas</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Ofertas</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Combos</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
